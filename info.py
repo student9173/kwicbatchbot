8a07a25737c6af1a6b78a6b6f68f7d7e428a745f
kwicbot78 <115940699+kwicbot78@users.noreply.github.com>
2022-11-07 12:55:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '16929529') 
-API_HASH = environ('API_HASH', 'd7b9e677664d2c87db4c4c0205b6e18f') 
-BOT_TOKEN = environ('BOT_TOKEN', '5474689804:AAFQv7-eR9mfNtsmWy_ZOpUTLImJfhbG6r0') 
+API_ID = int(environ.get('API_ID', '16929529') 
+API_HASH = environ.get('API_HASH', 'd7b9e677664d2c87db4c4c0205b6e18f') 
+BOT_TOKEN = environ.get('BOT_TOKEN', '5474689804:AAFQv7-eR9mfNtsmWy_ZOpUTLImJfhbG6r0') 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
