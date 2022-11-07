a29b93d8f72acf28f2c58aa19667dd9243d60a14
kwicbot78 <115940699+kwicbot78@users.noreply.github.com>
2022-11-07 12:31:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kwicbatchEva/kwicbatchbot.git /EvaMaria
+  git clone https://github.com/kwicbot78/kwicbatchbot2023.git /kwicbatchbot2023
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /kwicbatchbot2023
 fi
-cd /EvaMaria
+cd /kwicbatchbot2023
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
